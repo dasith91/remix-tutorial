c72e17708dd90418882f68580f157da2a5591cb0
dasith91 <dev.dasiththaraka@gmail.com>
2024-01-31 18:36:01 +0530

    Create remix project

--- app/root.tsx
+++ app/root.tsx
@@ -2,12 +2,35 @@ import {
   Form,
   Links,
   LiveReload,
+  Link,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
+import { LinksFunction, json, redirect } from "@remix-run/node";
+import appStyleHref from "./app.css";
+import { createEmptyContact, getContacts } from "./data";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStyleHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return redirect(`contacts/${contact.id}/edit`);
+  // return json({ contact });
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -35,17 +58,33 @@ export default function App() {
             </Form>
           </div>
           <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? <span>★</span> : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
           </nav>
         </div>
-
+        <div id="detail">
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
